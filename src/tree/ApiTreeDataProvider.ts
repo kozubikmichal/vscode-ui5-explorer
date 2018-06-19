1b91a2105cfe4be07f5edc2942d5afa400f37a29
Michal Kozubik <michal.kozubik@sap.com>
2018-06-19 13:25:39 +0200

    Implement tree rendering

--- src/tree/ApiTreeDataProvider.ts
+++ src/tree/ApiTreeDataProvider.ts
@@ -1,26 +1,96 @@
 import * as vscode from "vscode";
 import ApiTreeItem from "./ApiTreeItem";
 import { Storage } from "../api/Storage";
+import { SymbolKind, SymbolVisibility } from "../api/IApiReference";
+
+interface IApiTree {
+	[key: string]: ApiTreeItem;
+}
 
 export default class ApiTreeDataProvider implements vscode.TreeDataProvider<ApiTreeItem> {
-	private storage = new Storage();
+	private tree!: IApiTree;
+
+	constructor(
+		private storage: Storage = new Storage()
+	) { }
 
 	onDidChangeTreeData?: vscode.Event<ApiTreeItem | null | undefined> | undefined;
 
 	getTreeItem(element: ApiTreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		element.update();
 		return element;
 	}
 
 	async getChildren(element?: ApiTreeItem | undefined): Promise<ApiTreeItem[]> {
-		if (element) {
-			return [];
+		let tree = await this.parseTree();
+
+		if (!element) {
+			return this.getRoots(tree);
+		}
+
+		return this.getChildrenFor(tree, element);
+	}
+
+	private async getRoots(tree: IApiTree): Promise<ApiTreeItem[]> {
+		return Object.keys(tree)
+			.map(key => tree[key])
+			.filter(item => item.IsRoot)
+			.sort(this.sorter);
+	}
+
+	private async getChildrenFor(tree: IApiTree, element: ApiTreeItem): Promise<ApiTreeItem[]> {
+		return element.children.sort(this.sorter);
+	}
+
+	private async parseTree(): Promise<IApiTree> {
+		if (this.tree) {
+			return this.tree;
 		}
 
 		let index = await this.storage.getApiIndex();
 
-		return index.symbols.map(item => new ApiTreeItem(
-			item.name,
-			vscode.TreeItemCollapsibleState.None
-		));
+		this.tree = index.symbols
+			.map(symbol => new ApiTreeItem(symbol))
+			.reduce((all, item) => {
+				let name = item.symbol.name;
+				let parent = name.includes(".") ? name.replace(/\.[^.]*$/, "") : null;
+
+				if (item.symbol.kind !== SymbolKind.Namespace && parent) {
+
+					all[parent] = all[parent] || this.createWrapperItem(parent);
+
+					if (all[parent]) {
+						all[parent].children.push(item);
+						item.parent = all[parent];
+					}
+				}
+
+				if (!all[name]) {
+					all[name] = item;
+				}
+
+				all[name].symbol = item.symbol;
+				item.wrapperOnly = false;
+
+				return all;
+			}, {} as IApiTree);
+
+		return this.tree;
+	}
+
+	private sorter(a: ApiTreeItem, b: ApiTreeItem): number {
+		return (a.label || "").toLowerCase() > (b.label || "").toLowerCase() ? 1 : -1;
+	}
+
+	private createWrapperItem(label: string): ApiTreeItem {
+		let item = new ApiTreeItem({
+			name: label,
+			kind: SymbolKind.Namespace,
+			lib: "",
+			visibility: SymbolVisibility.Public
+		});
+
+		item.wrapperOnly = true;
+		return item;
 	}
 }
--- src/tree/ApiTreeItem.ts
+++ src/tree/ApiTreeItem.ts
@@ -1,5 +1,31 @@
 import * as vscode from "vscode";
 
-export default class ApiTreeItem extends vscode.TreeItem {
+import { IApiReferenceIndexSymbol } from "../api/IApiReference";
 
+export default class ApiTreeItem extends vscode.TreeItem {
+	public children: ApiTreeItem[] = [];
+	public parent!: ApiTreeItem;
+	public symbol!: IApiReferenceIndexSymbol;
+	public wrapperOnly: boolean = false;
+
+	private get HasChildren() {
+		return this.children.length > 0;
+	}
+
+	public get IsRoot() {
+		return this.parent === null || this.parent === undefined;
+	}
+
+	constructor(symbol?: IApiReferenceIndexSymbol) {
+		super(symbol ? symbol.name : "");
+
+		this.symbol = symbol || {} as IApiReferenceIndexSymbol;
+		this.update();
+	}
+
+	public update() {
+		this.id = this.symbol.name;
+		this.label = this.IsRoot ? this.symbol.name : this.symbol.name.replace(`${this.parent.symbol.name}.`, "");
+		this.collapsibleState = this.HasChildren ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+	}
 }
