02af899a2cd58ca49ef82ca33347b078ad03ef2b
Michal Kozubik <michal.kozubik@sap.com>
2018-06-22 18:00:43 +0200

    Add search button

--- src/Search.ts
+++ src/Search.ts
@@ -18,7 +18,7 @@ export default class Search {
 				};
 			}), {
 				canPickMany: false,
-				placeHolder: "Start writing..."
+				placeHolder: "Start typing..."
 			}
 		).then(selected => this.searchResultHandler(selected));
 	}
